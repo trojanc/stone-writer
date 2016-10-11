a83df48e382481435e535b2150a8227a2f574f4d
Charl Thiem <charl.thiem@gmail.com>
2016-10-11 21:21:34 +0200

    JEXL expression

---
+++ src/main/java/coza/trojanc/stonewriter/shared/DynamicType.java
@@ -0,0 +1,11 @@
+package coza.trojanc.stonewriter.shared;
+
+/**
+ * Created by Charl-PC on 2016-10-11.
+ */
+public enum DynamicType {
+	Date,
+	String,
+	Number,
+	Decimal
+}
--- src/main/java/coza/trojanc/stonewriter/template/fields/DynamicText.java
+++ src/main/java/coza/trojanc/stonewriter/template/fields/DynamicText.java
@@ -1,5 +1,7 @@
 package coza.trojanc.stonewriter.template.fields;
 
+import coza.trojanc.stonewriter.shared.DynamicType;
+
 /**
  * A class representing simple static text to be printed
  */
@@ -10,6 +12,33 @@ public class DynamicText extends AbstractTextItem{
 	 * Text to be printed
 	 */
 	private String expression;
+
+	private DynamicType type = DynamicType.String;
+
+	/**
+	 * Formatting to apply to value
+	 * The following fomatters will be applied to each of these types
+	 * <table>
+	 *    <tr>
+	 *        <th>DynamicType</th>
+	 *        <th>Class</th>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>Date</td>
+	 *         <td>SimpleDateFormat</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>Number</td>
+	 *         <td>String.format()</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>Decimal</td>
+	 *         <td>DecimalFormat</td>
+	 *     </tr>
+	 * </table>
+	 */
+	private String formatting;
+
 	/**
 	 * Text constructor.
 	 */
@@ -22,5 +51,21 @@ public class DynamicText extends AbstractTextItem{
 
 	public void setExpression(String expression) {
 		this.expression = expression;
+	}
+
+	public DynamicType getType() {
+		return type;
+	}
+
+	public void setType(DynamicType type) {
+		this.type = type;
+	}
+
+	public String getFormatting() {
+		return formatting;
+	}
+
+	public void setFormatting(String formatting) {
+		this.formatting = formatting;
 	}
 }
